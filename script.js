fdd54fe695edae7bec404c64bae584a4e0d74d31
Slava <slava.teplov.2014@mail.ru>
2022-07-05 20:11:04 +0300

    Updates

--- script.js
+++ script.js
@@ -6,15 +6,25 @@ const card = document.querySelector('.right-part')
 const cardError = document.querySelector('.error')
 let count = document.querySelector('.count')
 let price = document.querySelector('.price')
-
+let saleBtn = document.querySelector('.saleItemButton')
+let saleInput = document.querySelector('.sale')
 let itemArr = []
+let saleValue= parseInt(saleInput.value)
+let saleError = document.querySelector('.error-sale')
+let saleNew = document.querySelector('.sale-new')
+
+
+
 
 
 function CardItem(name,code,cost){
     this.name=name;
     this.code=code;
     this.cost=cost;
+    this.sale = ""
 }
+
+
 
 let deleteBtn= document.querySelector(".deleteElem")
 
@@ -39,7 +49,8 @@ function htmlElem(item,index){
     return `<div>
     <h2>${item.name}</h2>
     <p>${item.code}</p>
-    <p>${item.cost}</p>
+    <p class="forSale"></p>
+    <p>${item.cost} рублей</p>
     <button onclick="deleteElem(${index})" class="deleteElem">Удалить</button>
     </div>`
 }
@@ -47,9 +58,11 @@ function htmlElem(item,index){
 
 function createElem(){
     itemArr.length==0?card.innerHTML="<p class='list'>Список пуст</p>":card.innerHTML=''
-    itemArr.forEach((item,index)=>{card.innerHTML += htmlElem(item,index)})
+    itemArr.forEach((item,index)=>{card.innerHTML += htmlElem(item,index)
+    })
 
 }
+
 
 function deleteElem(index){
     itemArr.splice(index,1)
@@ -58,9 +71,29 @@ function deleteElem(index){
     createSum()
 }
 
+
 function createSum(){
     let sumCount=0
     itemArr.forEach((item)=>sumCount+=parseInt(item.cost))
     price.innerHTML=`${sumCount} р`
+}
+
+
+saleBtn.addEventListener('click',function(){
+    if(!saleInput.value){saleError.innerHTML="Недопустимое значение скидки"}
+    else{
+        saleError.innerHTML=""
+        totalOfSale()
+    }
+})
+
+function totalOfSale(){
+    let saleItem = document.querySelectorAll('.forSale')
+    
+    itemArr.forEach((item,index)=>{saleItem[index].innerHTML=item.cost-item.cost*parseInt(saleInput.value)/100
+    let bottom=saleItem[index].nextSibling.nextSibling
+    bottom.classList.add("throught")
+})
+
 }
 
